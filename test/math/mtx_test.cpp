382adb2999faa7721ad225721bafb60dbe6874cc
Ben Scott <gamepoet@gmail.com>
2011-09-03 11:15:13 -0700

    Rename math.h to vecmath.h to avoid include collisions.

--- test/math/mtx_test.cpp
+++ test/math/mtx_test.cpp
@@ -24,7 +24,7 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 #include <unittest++/UnitTest++.h>
-#include <blink/math.h>
+#include <blink/vecmath.h>
 
 SUITE(math) {
   //----------------------------------------------------------------------------
